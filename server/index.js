767bd85a6df54e5d00e7d1b85e4d1259967b8cb1
khalekarakash05 <akashkhalekar2021.it@mmcoe.edu.in>
2024-07-24 00:23:23 +0530

    adding cors

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://study-notion-ikelpqf6d-akashs-projects-0345b2d2.vercel.app/",
         credentials: true,
     })
 );
